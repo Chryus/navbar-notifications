f3b2c6b8a0802749af2a1bd6d6c33a77f6b58f75
Chris <chris.haack@me.com>
2016-10-18 15:36:28 -0400

    add resource routes, controllers, outstanding relation declarations, and model scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :notifcations, foreign_key: :recipient_id
+  has_many :forum_threads
+  has_many :forum_posts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :forum_threads do
+    resources :forum_posts
+  end
+
+  resources :notifications do
+    collection do
+      post :mark_as_read
+    end
+  end
   devise_scope :user do
     # authenticated :user do
     #   root 'home#index', as: :authenticated_root
     # end
-
     unauthenticated do
       root to: 'devise/sessions#new'
     end
